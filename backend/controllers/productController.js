c7aa2b296c12d1bbac939fa946b656c71f504c0e
mudit <muditshukla010@gmail.com>
2022-01-16 20:29:52 +0530

    added get poduct details api

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,53 +5,67 @@ const Product = require("../models/productModel");
 exports.createProduct = async (req, res) => {
     const newProduct = await Product.create(req.body);
     res.status(201).json({
-        success : true,
+        success: true,
         newProduct
     })
 }
 
 
 exports.getAllProducts = async (req, res) => {
-    const products = await Product.find(); 
+    const products = await Product.find();
     res.status(201).json({
-        success : true,
+        success: true,
         products
     })
 }
 
 exports.updateProduct = async (req, res) => {
     const product = await Product.findById(req.params.id);
-    if(!product){
+    if (!product) {
         return res.status(500).json({
-            success : false,
-            message : "product not found"
+            success: false,
+            message: "product not found"
         })
     }
 
-    const updatedProduct  = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, {
         new: true,
-        runValidators : true,
-        useFindAndModify : false,
+        runValidators: true,
+        useFindAndModify: false,
     })
 
     res.status(200).json({
-        success : true,
+        success: true,
         product
     })
 }
 
 exports.deleteProduct = async (req, res) => {
     let product = await Product.findById(req.params.id);
+    if (!product) {
+        return res.status(500).json({
+            success: false,
+            message: "element not found"
+        })
+    } else {
+        await product.remove();
+        res.status(200).json({
+            success: true,
+            message: "product deleted"
+        })
+    }
+}
+
+exports.getProductDetails = async (req, res) => {
+    const product = await Product.findById(req.params.id);
     if(!product){
         return res.status(500).json({
             success : false,
-            message : "element not found" 
+            message : "element not found"
         })
-    }else{
-        await product.remove();
-        res.status(200).json({
+    }
+    else return res.status(200).json({
         success : true,
-        message : "product deleted"
+        product
     })
-}
 }
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const { put } = require("../app");
-const { getAllProducts, createProduct, updateProduct, deleteProduct } = require("../controllers/productController");
+const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails } = require("../controllers/productController");
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
 router.route("/products/newProduct").post(createProduct);
 router.route("/products/updateProduct/:id").put(updateProduct);
-router.route("/products/deleteProduct/:id").delete(deleteProduct);
-
+router.route("/products/deleteProduct/:id").delete(deleteProduct).get(getProductDetails);
 
 module.exports = router;
